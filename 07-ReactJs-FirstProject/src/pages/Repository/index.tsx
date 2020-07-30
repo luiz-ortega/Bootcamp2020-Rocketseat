9714b0ff25d793679ecb7905d0609d5e18a19730
luiz-ortega <luizngust@hotmail.com>
2020-07-30 00:54:17 -0300

    style: repository details.

--- 07-ReactJs-FirstProject/src/pages/Repository/index.tsx
+++ 07-ReactJs-FirstProject/src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -10,10 +15,48 @@ const Repository: React.FC = () => {
 
   return (
     <>
-      <h1>
-        Repository:
-        {params.repository}
-      </h1>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={20} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="" />
+          <div>
+            <strong>rockestseat</strong>
+            <p>descricao</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1800</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1800</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1800</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="asdas">
+          <div>
+            <strong>asdasd</strong>
+            <p>asdasd</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
     </>
   );
 };
