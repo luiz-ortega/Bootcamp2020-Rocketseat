43a63e2d2e023d77fece748f1b81008f0457dfc1
luiz-ortega <luizngust@hotmail.com>
2020-08-03 22:09:51 -0300

    feat: Input and Button componetization.

--- 08-reactjs-gobarber/src/pages/SignIn/index.tsx
+++ 08-reactjs-gobarber/src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
@@ -12,10 +15,15 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu logon</h1>
 
-        <input placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
+        <Input icon={FiMail} name="email" placeholder="E-mail" />
+        <Input
+          icon={FiLock}
+          name="password"
+          type="password"
+          placeholder="Senha"
+        />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
