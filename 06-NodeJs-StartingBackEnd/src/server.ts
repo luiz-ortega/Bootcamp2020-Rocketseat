974140d1335519e619c683735abb6750ffb7ceed
luiz-ortega <luizngust@hotmail.com>
2020-08-06 21:27:58 -0300

    feat: cors

--- 06-NodeJs-StartingBackEnd/src/server.ts
+++ 06-NodeJs-StartingBackEnd/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -9,9 +10,10 @@ import AppError from './errors/AppError';
 import './database';
 
 const app = express();
+
 app.use('/files', express.static(uploadConfig.directory));
 app.use(express.json());
-
+app.use(cors());
 app.use(routes);
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     if (err instanceof AppError) {
