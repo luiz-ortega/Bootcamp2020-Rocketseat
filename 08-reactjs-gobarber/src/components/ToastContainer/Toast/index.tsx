93fa00885613a98c67d11e5a1673bd1214a954e2
luiz-ortega <luizngust@hotmail.com>
2020-08-08 17:37:29 -0300

    style: toast animations.

--- 08-reactjs-gobarber/src/components/ToastContainer/Toast/index.tsx
+++ 08-reactjs-gobarber/src/components/ToastContainer/Toast/index.tsx
@@ -4,7 +4,6 @@ import {
   FiXCircle,
   FiCheckCircle,
   FiInfo,
-  FiCheck,
 } from 'react-icons/fi';
 
 import { ToastMessage, useToast } from '../../../hooks/toast';
@@ -13,6 +12,7 @@ import { Container } from './styles';
 
 interface ToastProps {
   message: ToastMessage;
+  style: object;
 }
 
 const icons = {
@@ -21,7 +21,7 @@ const icons = {
   success: <FiCheckCircle size={24} />,
 };
 
-const Toast: React.FC<ToastProps> = ({ message }) => {
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
   const { removeToast } = useToast();
 
   useEffect(() => {
@@ -35,9 +35,12 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
   }, [removeToast, message.id]);
 
   return (
-    <Container type={message.type} hasDescription={!!message.description}>
+    <Container
+      type={message.type}
+      hasDescription={!!message.description}
+      style={style}
+    >
       {icons[message.type || 'info']}
-
       <div>
         <strong>{message.title}</strong>
         {message.description && <p>{message.description}</p>}
