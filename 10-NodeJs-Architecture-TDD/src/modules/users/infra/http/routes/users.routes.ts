9648470078b42ab3d7491b480a61b03a933db9d6
luiz-ortega <luizngust@hotmail.com>
2020-08-21 01:28:45 -0300

    refactor: adding controllers to modules.

--- 10-NodeJs-Architecture-TDD/src/modules/users/infra/http/routes/users.routes.ts
+++ 10-NodeJs-Architecture-TDD/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,48 +1,24 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
-import { container } from 'tsyringe';
 
-import CreateUserService from '@modules/users/services/CreateuserService';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import UsersController from '../controllers/UsersController';
+import UsersAvatarController from '../controllers/UsersAvatarController';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', async (request, response) => {
-    const { name, email, password } = request.body;
+const usersController = new UsersController();
+const usersAvatarController = new UsersAvatarController();
 
-    const createuser = container.resolve(CreateUserService);
-
-    const user = await createuser.execute({
-        name,
-        email,
-        password,
-    });
-
-    delete user.password;
-
-    return response.json(user);
-});
+usersRouter.post('/', usersController.create);
 
 usersRouter.patch(
     '/avatar',
     ensureAuthenticated,
     upload.single('avatar'),
-    async (request, response) => {
-        const updateUserAvatar = container.resolve(UpdateUserAvatarService);
-
-        const user = await updateUserAvatar.execute({
-            user_id: request.user.id,
-            avatarFileName: request.file.filename,
-        });
-
-        delete user.password;
-
-        return response.json(user);
-    },
+    usersAvatarController.update,
 );
 
 export default usersRouter;
