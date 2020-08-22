9c7ca56d890a61c7196cc50466c9d8e0d5d7520c
luiz-ortega <luizngust@hotmail.com>
2020-08-22 18:17:13 -0300

    test: authenticate user

---
+++ 10-NodeJs-Architecture-TDD/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,27 @@
+// import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import AuthenticateUserService from './AuthenticateUserService';
+import CreateUserService from './CreateuserService';
+
+describe('Authenticate User', () => {
+    it('should be able to authenticate', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const createUser = new CreateUserService(fakeUsersRepository);
+        const authenticateUser = new AuthenticateUserService(
+            fakeUsersRepository,
+        );
+
+        await createUser.execute({
+            name: 'Joe Doe',
+            email: 'email@email.com',
+            password: '123456',
+        });
+
+        const response = await authenticateUser.execute({
+            email: 'email@email.com',
+            password: '123456',
+        });
+
+        expect(response).toHaveProperty('token');
+    });
+});
