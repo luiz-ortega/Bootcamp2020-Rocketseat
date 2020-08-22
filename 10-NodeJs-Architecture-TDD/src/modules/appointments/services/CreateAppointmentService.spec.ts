8aad4fbedc2d7937d049f31b7287343f287b542f
luiz-ortega <luizngust@hotmail.com>
2020-08-22 17:18:49 -0300

    test: appointments and coverage

--- 10-NodeJs-Architecture-TDD/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ 10-NodeJs-Architecture-TDD/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentsService from './CreateAppointmentService';
 
@@ -15,5 +16,26 @@ describe('CreateAppointment', () => {
 
         expect(appointment).toHaveProperty('id');
         expect(appointment.provider_id).toBe('123456');
+    });
+
+    it('should not be able to create two appointments at the same date', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentsService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointmentsDate = new Date();
+
+        await createAppointment.execute({
+            date: appointmentsDate,
+            provider_id: '123456',
+        });
+
+        expect(
+            createAppointment.execute({
+                date: appointmentsDate,
+                provider_id: '123456',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
     });
 });
