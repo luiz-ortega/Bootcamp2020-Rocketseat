1823717006d785eaac3de3fbdcd04ae2c869c5cc
luiz-ortega <luizngust@hotmail.com>
2020-08-22 18:46:36 -0300

    refactor: hashProvider and tests.

--- 10-NodeJs-Architecture-TDD/src/modules/users/services/AuthenticateUserService.spec.ts
+++ 10-NodeJs-Architecture-TDD/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,17 +1,24 @@
 // import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticateUserService from './AuthenticateUserService';
 import CreateUserService from './CreateuserService';
 
 describe('Authenticate User', () => {
     it('should be able to authenticate', async () => {
         const fakeUsersRepository = new FakeUsersRepository();
-        const createUser = new CreateUserService(fakeUsersRepository);
+        const fakeHashProvider = new FakeHashProvider();
+
+        const createUser = new CreateUserService(
+            fakeUsersRepository,
+            fakeHashProvider,
+        );
         const authenticateUser = new AuthenticateUserService(
             fakeUsersRepository,
+            fakeHashProvider,
         );
 
-        await createUser.execute({
+        const user = await createUser.execute({
             name: 'Joe Doe',
             email: 'email@email.com',
             password: '123456',
@@ -23,5 +30,6 @@ describe('Authenticate User', () => {
         });
 
         expect(response).toHaveProperty('token');
+        expect(response.user).toEqual(user);
     });
 });
--- 10-NodeJs-Architecture-TDD/src/modules/users/services/CreateuserService.ts
+++ 10-NodeJs-Architecture-TDD/src/modules/users/services/CreateuserService.ts
@@ -1,10 +1,8 @@
-import { hash } from 'bcryptjs';
-
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import { injectable, inject } from 'tsyringe';
-
 import User from '@modules/users/infra/typeorm/entities/User';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 interface IRequest {
     name: string;
@@ -17,6 +15,9 @@ class CreateUserService {
     constructor(
         @inject('UsersRepository')
         private usersRepository: IUsersRepository,
+
+        @inject('HashProvider')
+        private hashProvider: IHashProvider,
     ) { }
 
     public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -26,7 +27,7 @@ class CreateUserService {
             throw new AppError('Email address already used.');
         }
 
-        const hashedPassword = await hash(password, 8);
+        const hashedPassword = await this.hashProvider.generateHash(password);
 
         const user = await this.usersRepository.create({
             name,
