c3ed274be6d423bb0b54433fca4b0c2f56db5f53
luiz-ortega <luizngust@hotmail.com>
2020-08-28 00:43:09 -0300

    refactor: UpdateUserAvatarService test.

--- 11-NodeJs-ContinuingBackEnd/src/modules/users/services/ResetPasswordService.spec.ts
+++ 11-NodeJs-ContinuingBackEnd/src/modules/users/services/ResetPasswordService.spec.ts
@@ -46,7 +46,7 @@ describe('ResetPassword', () => {
     });
 
     it('should not be able to reset the password with non-existing token', async () => {
-        expect(
+        await expect(
             resetPassword.execute({
                 token: 'non-existing-token',
                 password: '123456',
@@ -59,7 +59,7 @@ describe('ResetPassword', () => {
             'non-existing-user',
         );
 
-        expect(
+        await expect(
             resetPassword.execute({
                 token,
                 password: '123456',
