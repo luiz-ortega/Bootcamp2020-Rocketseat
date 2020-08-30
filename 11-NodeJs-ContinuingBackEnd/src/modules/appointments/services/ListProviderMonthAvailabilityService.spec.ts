9495375a5349bd4ea1d03624d2b7a3517123c2f8
luiz-ortega <luizngust@hotmail.com>
2020-08-30 02:37:24 -0300

    test: list provider`s available dates

--- 11-NodeJs-ContinuingBackEnd/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ 11-NodeJs-ContinuingBackEnd/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -20,7 +20,52 @@ describe('ListProviderMonthAvailability', () => {
 
         await fakeAppointmentsRepository.create({
             provider_id: 'user',
+            date: new Date(2020, 4, 20, 8, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 9, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
             date: new Date(2020, 4, 20, 10, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 11, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 12, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 13, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 14, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 15, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 16, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 17, 0, 0),
         });
 
         await fakeAppointmentsRepository.create({
@@ -29,7 +74,7 @@ describe('ListProviderMonthAvailability', () => {
         });
 
         const availability = await listProviderMonthAvailability.execute({
-            user_id: 'user',
+            provider_id: 'user',
             year: 2020,
             month: 5,
         });
@@ -38,7 +83,7 @@ describe('ListProviderMonthAvailability', () => {
             expect.arrayContaining([
                 { day: 19, available: true },
                 { day: 20, available: false },
-                { day: 21, available: false },
+                { day: 21, available: true },
                 { day: 22, available: true },
             ]),
         );
--- 11-NodeJs-ContinuingBackEnd/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ 11-NodeJs-ContinuingBackEnd/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,11 +1,11 @@
 import { injectable, inject } from 'tsyringe';
 
-import User from '@modules/users/infra/typeorm/entities/User';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
-    user_id: string;
+    provider_id: string;
     month: number;
     year: number;
 }
@@ -23,26 +23,37 @@ class ListProviderMonthAvailabilityService {
     ) { }
 
     public async execute({
-        user_id,
+        provider_id,
         year,
         month,
     }: IRequest): Promise<IResponse> {
         const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
             {
-                provider_id: user_id,
+                provider_id,
                 year,
                 month,
             },
         );
 
-        console.log(appointments);
+        const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
-        return [
-            {
-                day: 1,
-                available: false,
-            },
-        ];
+        const eachDayArray = Array.from(
+            { length: numberOfDaysInMonth },
+            (_, index) => index + 1,
+        );
+
+        const availability = eachDayArray.map(day => {
+            const appointmentsInDay = appointments.filter(appointment => {
+                return getDate(appointment.date) === day;
+            });
+
+            return {
+                day,
+                available: appointmentsInDay.length < 10,
+            };
+        });
+
+        return availability;
     }
 }
 
