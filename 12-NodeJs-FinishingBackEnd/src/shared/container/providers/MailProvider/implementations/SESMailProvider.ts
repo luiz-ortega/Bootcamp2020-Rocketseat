1836f1a4f9d51899157c6207b392b8132cfbeb33
luiz-ortega <luizngust@hotmail.com>
2020-09-03 23:49:10 -0300

    feat: send email service with AWS-SES.

---
+++ 12-NodeJs-FinishingBackEnd/src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -0,0 +1,44 @@
+import nodemailer, { Transporter } from 'nodemailer';
+import aws from 'aws-sdk';
+import { injectable, inject } from 'tsyringe';
+
+import IMailProvider from '../models/IMailProvider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
+
+import IMailTemplateProvider from '../../MailTemplateProvider/models/IMailTemplateProvider';
+
+@injectable()
+export default class SESMailProvider implements IMailProvider {
+    private client: Transporter;
+
+    constructor(
+        @inject('MailTemplateProvider')
+        private mailTemplateProvider: IMailTemplateProvider,
+    ) {
+        this.client = nodemailer.createTransport({
+            SES: new aws.SES({
+                apiVersion: '2010-12-01',
+            }),
+        });
+    }
+
+    public async sendMail({
+        to,
+        from,
+        subject,
+        templateData,
+    }: ISendMailDTO): Promise<void> {
+        await this.client.sendMail({
+            from: {
+                name: from?.name || 'Equipe GoBarber',
+                address: from?.email || 'equipe@gobarber.com.br',
+            },
+            to: {
+                name: to.name,
+                address: to.email,
+            },
+            subject,
+            html: await this.mailTemplateProvider.parse(templateData),
+        });
+    }
+}
