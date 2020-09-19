d1924de19149d4e76fffcb02f725fe21e77f8ece
luiz-ortega <luizngust@hotmail.com>
2020-09-19 20:31:44 -0300

    style: dashboard page layout.

--- 13-reactjs-FinishingWebApp/src/pages/Dashboard/index.tsx
+++ 13-reactjs-FinishingWebApp/src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+} from './styles';
 
 import { useAuth } from '../../hooks/auth';
 
@@ -29,6 +38,33 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars1.githubusercontent.com/u/31256653?s=460&u=4f439431fc5e3b9d9603b3fbd8651748710d9eb5&v=4"
+                alt="Luiz Ortega"
+              />
+              <strong>Luiz Ortega</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
