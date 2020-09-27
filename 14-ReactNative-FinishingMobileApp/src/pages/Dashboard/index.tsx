4775f2ac05f536d640161519896ea539106384bc
luiz-ortega <luizngust@hotmail.com>
2020-09-27 20:14:30 -0300

    feat: Dashboard header with logged user info and Profile navigation.

--- 14-ReactNative-FinishingMobileApp/src/pages/Dashboard/index.tsx
+++ 14-ReactNative-FinishingMobileApp/src/pages/Dashboard/index.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import {Button, View, Text} from 'react-native';
+import React, { useCallback } from 'react';
+import { Container, Header, HeaderTitle, UserName,ProfileButton,UserAvatar  } from './styles'
 import { useAuth } from '../../hooks/auth'
+import Button from '../../components/Button';
+import { useNavigation } from '@react-navigation/native';
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+  const { navigate } = useNavigation()
+
+  const navigationProfile = useCallback(() => {
+    navigate('Profile')
+  }, [navigate])
 
   return (
-    <View style={{ flex:1, justifyContent: 'center' }}>
-      <Text style={{ color: "#fff" }}>Dashboard</Text>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {"\n"}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigationProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+
+      </Header>
+      {/* <Button onPress={signOut}>LOGOUT</Button> */}
+    </Container>
   )
 };
 
