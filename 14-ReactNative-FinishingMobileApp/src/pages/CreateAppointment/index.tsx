c5e9d6ac4f56846f842ad9062770e81185d8c29c
luiz-ortega <luizngust@hotmail.com>
2020-10-01 19:31:20 -0300

    feat: get provider availability.

--- 14-ReactNative-FinishingMobileApp/src/pages/CreateAppointment/index.tsx
+++ 14-ReactNative-FinishingMobileApp/src/pages/CreateAppointment/index.tsx
@@ -23,6 +23,11 @@ export interface Provider {
   avatar_url: string;
 }
 
+interface AvailabilityItem {
+  hour: number;
+  available: boolean;
+}
+
 const avatarPlaceHolder = 'https://api.adorable.io/avatars/285/abott@adorable.png'
 
 const CreateAppointment: React.FC = () => {
@@ -31,6 +36,7 @@ const CreateAppointment: React.FC = () => {
   const { goBack } = useNavigation()
   const routeParams = route.params as RouteParams;
 
+  const [availability, setAvailability] = useState<AvailabilityItem[]>([]);
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [providers, setProviders] = useState<Provider[]>([]);
@@ -41,6 +47,18 @@ const CreateAppointment: React.FC = () => {
       setProviders(response.data)
     });
   }, []);
+
+  useEffect(() => {
+    api.get(`providers/${selectedProvider}/day-availability`, {
+      params: {
+        year: selectedDate.getFullYear(),
+        month: selectedDate.getMonth() + 1,
+        day: selectedDate.getDate()
+      }
+    }).then(response => {
+      setAvailability(response.data)
+    });
+  }, [selectedDate, selectedProvider]);
 
   const navigateBack = useCallback(() => {
     goBack()
