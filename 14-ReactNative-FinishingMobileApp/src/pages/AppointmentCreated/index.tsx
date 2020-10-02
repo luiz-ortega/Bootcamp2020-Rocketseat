8c93cfb33ab3306da02bba0e35203a5f47eec46d
luiz-ortega <luizngust@hotmail.com>
2020-10-02 20:25:17 -0300

    feat: create appointment success page.

--- 14-ReactNative-FinishingMobileApp/src/pages/AppointmentCreated/index.tsx
+++ 14-ReactNative-FinishingMobileApp/src/pages/AppointmentCreated/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import {Button, View, Text} from 'react-native';
-import { useAuth } from '../../hooks/auth'
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { Container, Title, Description, OkButton, OkButtonText } from './styles';
 
 const AppointmentCreated: React.FC = () => {
-  const { signOut } = useAuth();
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0
+    })
+  }, [reset])
 
   return (
-    <View style={{ flex:1, justifyContent: 'center' }}>
-      <Text style={{ color: "#fff" }}>AppointmentCreated</Text>
-    </View>
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>Terça, dia 14 de março de 2020 às 20h00</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
   )
 };
 
