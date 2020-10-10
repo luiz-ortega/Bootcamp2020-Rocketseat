09d2cc2aab482078b35098416a3a5052a6adc72a
luiz-ortega <luizngust@hotmail.com>
2020-10-09 22:00:37 -0300

    test: render focus style.

--- 15-reactjs-gobarber-TDD/src/__tests__/components/input.spec.tsx
+++ 15-reactjs-gobarber-TDD/src/__tests__/components/input.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import Input from '../../components/Input';
 
 jest.mock('@unform/core', () => {
@@ -23,5 +23,21 @@ describe('Inout component', () => {
     );
 
     expect(getByPlaceholderText('E-mail')).toBeTruthy();
+  });
+
+  it('should render highlighted on input focus', () => {
+    const { getByPlaceholderText, getByTestId } = render(
+      <Input name="email" placeholder="E-mail" />,
+    );
+
+    const inpiutElement = getByPlaceholderText('E-mail');
+    const containerElement = getByTestId('input-container');
+
+    fireEvent.focus(inpiutElement);
+
+    waitFor(() => {
+      expect(containerElement).toHaveStyle('border-color: #ff9000;');
+      expect(containerElement).toHaveStyle('color: #ff9000;');
+    });
   });
 });
